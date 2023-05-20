6b4a5fcfd69d4c8a7eaa2aeb08a5b5b771a3a95d
Shivani Mishra <119618398+SHIVANIMISHRA123@users.noreply.github.com>
2023-05-20 14:50:55 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b998e7da4380e71e29cd860ed973a41b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
